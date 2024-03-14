dec3f7666234a23a5d5f731602ecc22ab7b2bd4a
Nikita Gupta <72199510+nikitaguptaaa@users.noreply.github.com>
2024-03-14 12:11:08 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId="144536"
+let accountEmail="ng567@google.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState;
+
+
+//accountId= 2//not allowed
+
+accountEmail="hg@gmail.com"
+accountPassword="8768"
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+
